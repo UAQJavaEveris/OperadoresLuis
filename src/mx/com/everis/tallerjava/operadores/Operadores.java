63384ab8e8f71e7ada63f65ea49f4941581decc7
luis martinez <luis martinez>
2016-11-14 18:45:06 -0600

    Segundo commit

--- src/mx/com/everis/tallerjava/operadores/Operadores.java
+++ src/mx/com/everis/tallerjava/operadores/Operadores.java
@@ -1,9 +1,35 @@
 package mx.com.everis.tallerjava.operadores;
 
+
 public class Operadores {
 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int valor1=10;
+		int valor2=5;
+		int valor3=4;
+		int valor4=8;
+		int valor5=4;
+		
+		if(valor1>valor2){
+			System.out.println(valor1+" es mayor que "+ valor2);
+		}		
+		if(valor2<valor4){
+			System.out.println(valor2+" es menor que "+ valor4);
+		}
+		if(valor3==valor5){
+			System.out.println(valor3+" es igual que "+ valor5);
+		}
+		if(valor1>=valor5){
+			System.out.println(valor1+" es mayor o igual que "+ valor5);			
+		}
+		if(valor3<=valor1){
+			System.out.println(valor3+" es menor o igual que "+ valor1);
+		}
+		if(valor2!=valor3){
+			System.out.println(valor2+" es diferente de "+ valor4);
+		}
+		
 
 	}
 
